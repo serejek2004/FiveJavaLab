c2ca83661789f605cff02bf028945382bb41363f
serejek2004 <serejek2004@gmail.com>
2023-04-27 18:07:19 +0300

    Fix naming

--- src/main/java/ua/lviv/iot/algo/part2/TextEditor.java
+++ src/main/java/ua/lviv/iot/algo/part2/TextEditor.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 public final class TextEditor {
     public String textEditor(String stringToEdit, String[] fragments) {
         for (String currentFragment : fragments) {
-            Pattern firstPattern = Pattern.compile("\\b\\w*" + currentFragment + "\\w*\\b");
-            Matcher firstMatcher = firstPattern.matcher(stringToEdit);
-            stringToEdit = firstMatcher.replaceAll("");
+            Pattern patternForDelFragments = Pattern.compile("\\b\\w*" + currentFragment + "\\w*\\b");
+            Matcher matcherForDelFragments = patternForDelFragments.matcher(stringToEdit);
+            stringToEdit = matcherForDelFragments.replaceAll("");
         }
         return stringToEdit;
     }
